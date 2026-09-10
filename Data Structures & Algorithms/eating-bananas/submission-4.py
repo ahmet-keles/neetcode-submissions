bdc2e40595e2bacced75d16c7167480b3d3523d7
Ahmet Keles <101959609+ahmet-keles@users.noreply.github.com>
2026-09-09 21:51:41 -0500

    Add: eating-bananas - submission-4

---
+++ Data Structures & Algorithms/eating-bananas/submission-4.py
@@ -0,0 +1,26 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        left = 1
+        right = max(piles)
+        res = right
+        while left <= right:
+            temp_hour_count = 0
+
+            middle = left + (right - left) // 2
+            for i in piles:
+                temp_num = i
+                if temp_num % middle > 0:
+                    temp_hour_count += 1
+                temp_hour_count += temp_num // middle
+
+            if temp_hour_count > h:
+                left = middle + 1
+            else:
+                res = min(middle, res)
+                right = middle - 1
+            
+        return res
+
+
+
+        
