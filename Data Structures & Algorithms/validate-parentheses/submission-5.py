6f64d9f961d138443965d296b873f65c76a4aa56
Ahmet Keles <101959609+ahmet-keles@users.noreply.github.com>
2026-09-14 17:44:59 -0500

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for i in range(len(s)):
+
+            if s[i] == '(' or s[i] == '[' or s[i] == '{':
+                stack.append(s[i])
+                continue
+
+            if not stack:
+                return False
+
+            c = stack.pop()
+            if s[i] != ')' and c == '(' or s[i] != ']' and c == '[' or s[i] != '}' and c == '{':
+                return False
+
+        if not stack:
+            return True
+
+        return False
